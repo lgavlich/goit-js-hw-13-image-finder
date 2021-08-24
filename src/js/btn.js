6a11f4aab794012b6b96a5d9a576736714a85fd3
lgavlich <lgavlich@gmail.com>
2021-08-24 21:38:48 +0300

    Create btn.js

---
+++ src/js/btn.js
@@ -0,0 +1,27 @@
+export default class LoadMoreButton{
+    constructor({ selector, hidden = false }) {
+        this.refs = this.getRefs(selector);
+        if (hidden) {
+            this.hide();
+        }
+    };
+    getRefs(selector) {
+        const refs = {};
+        refs.button = document.querySelector(selector);
+        refs.label = refs.button.querySelector('.label');
+        return refs;
+    }
+    hide() {
+        this.refs.button.classList.add('is-hidden');
+    }
+    show() {
+        this.refs.button.classList.remove('is-hidden');
+    }
+    enableBtn() {
+        this.refs.button.disabled = false;
+    }
+    disableBtn() {
+        this.refs.button.disabled = true;
+    }
+    
+}
